dd6e316e1352e4e29d0ece598b328cb3d98f1551
Jakub Perdoch <perdochjakub@gmail.com>
2025-07-23 22:20:58 +0200

    add BestSellersSection component and integrate with FeaturedCarousel

---
+++ src/components/BestSellersSection/index.tsx
@@ -0,0 +1,47 @@
+import { motion } from "framer-motion";
+import { itemVariants } from "@/utils/animations.ts";
+import { Image } from "@heroui/image";
+import { Button } from "@heroui/react";
+
+type CardProps = {
+  id: string;
+  image: string;
+  title: string;
+};
+
+interface BestSellersSectionProps {
+  cards: CardProps[];
+}
+
+export default function BestSellersSection({ cards }: BestSellersSectionProps) {
+  return (
+    <motion.section
+      variants={itemVariants}
+      className="flex flex-col gap-8 mt-20"
+    >
+      <h2 className="font-bold text-[4.6rem] leading-20">
+        Čerstvé dizajny,
+        <br /> okamžite k objednaniu.
+      </h2>
+
+      <motion.div variants={itemVariants} className="flex flex-row gap-4">
+        {cards &&
+          cards.length > 0 &&
+          cards.map((card) => (
+            <div key={card?.id} className="flex flex-col gap-4">
+              <Image
+                src={card?.image}
+                className="border-5 border-white rounded-[1.75rem] "
+                removeWrapper={true}
+                alt={card?.title}
+              />
+              <h3 className="text-2xl font-bold">{card?.title}</h3>
+              <Button className="!bg-[#232321] text-white py-2 px-4">
+                Zobraziť Produkt
+              </Button>
+            </div>
+          ))}
+      </motion.div>
+    </motion.section>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,29 @@ import { createFileRoute } from "@tanstack/react-router";
 import { motion } from "framer-motion";
 import { itemVariants } from "@/utils/animations.ts";
 import FeaturedCarousel from "@/components/FeaturedCarousel";
+import BestSellersSection from "@/components/BestSellersSection";
 
 export const Route = createFileRoute("/")({
   component: App,
 });
+
+const bestSellersCards = [
+  {
+    id: "1",
+    image: "../../public/bestseller-card.png",
+    title: "Kreatívny dizajn",
+  },
+  {
+    id: "2",
+    image: "../../public/bestseller-card.png",
+    title: "Elegantné tlače",
+  },
+  {
+    id: "3",
+    image: "../../public/bestseller-card.png",
+    title: "Moderné štýly",
+  },
+];
 
 function App() {
   return (
@@ -18,6 +37,7 @@ function App() {
       </motion.h1>
 
       <FeaturedCarousel />
+      <BestSellersSection cards={bestSellersCards} />
     </div>
   );
 }
